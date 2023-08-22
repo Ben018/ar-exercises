2d71adb00858b61166b11047c008eefee9267909
Benson <bensonwlee@gmail.com>
2023-08-22 14:46:43 -0400

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name but without other fields
+new_store = Store.new(name: store_name)
+
+# Display error messages if the creation is not successful
+unless new_store.save
+  puts "Error(s) occurred:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
