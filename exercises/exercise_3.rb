84f46b94f4ac846fd49a175b3da2f84d06d2c18c
Benson <bensonwlee@gmail.com>
2023-08-22 14:12:20 -0400

    Exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,10 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+  
+puts "Number of stores before: #{Store.count}"
+  
+@store3.destroy
+  
+puts "Number of stores after: #{Store.count}"
